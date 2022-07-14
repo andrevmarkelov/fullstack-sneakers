0a4a1a52fcf91639a0a697c60e7e555aa793bd3a
andrevmarkelov <andrev_markelov@mail.ru>
2022-07-14 20:42:22 +0300

    Added a message to favorites

--- client/src/pages/Favorite.jsx
+++ client/src/pages/Favorite.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import {Card} from "../components/Card";
 import AppContext from "../context";
 
 export const Favorite = () => {
   const {favorites, onAddToFavorite} = React.useContext(AppContext);
+  console.log(favorites);
 
   return (
     <div className="content">
       <div className="content-header">
         <h1>My Bookmarks</h1>
       </div>
-      <div className="sneakers">
+      {favorites.length > 0 ? <div className="sneakers">
         {favorites.map(item => (
           <Card
             key={item.id}
@@ -23,7 +25,14 @@ export const Favorite = () => {
             onFavorite={onAddToFavorite}
           />
         ))}
-      </div>
+      </div> : <div className="favorites-empty">
+        <h2>The list of favorite products is empty</h2>
+        <p>Most likely, you have not added products to your favorites.</p>
+        <p>In order to select a product, go to the main page.</p>
+        <Link to="/">
+          <button><img src="/img/arrow.svg" alt="Arrow"/>Back to Home Page</button>
+        </Link>
+      </div>}
     </div>
   )
 };
